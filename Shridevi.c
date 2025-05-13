b62c214b5fdfa9883d92c646470d2ea00d7442d9
srilekha016 <bpavithra890@gmail.com>
2025-05-13 15:45:00 +0530

    Create Shridevi.c

---
+++ Shridevi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int shridevi()
+{
+    printf("srilekha:\n");
+    printf("d-o-b:\n");
+    printf("age: \n");
+    printf("mobile number: \n");
+}
+int main()
+{
+    shridevi();
+    return 0;
+}
